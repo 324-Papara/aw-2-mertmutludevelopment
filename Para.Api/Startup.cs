5904efcb8c2a63b84e3fc1ab3511e5cecffcc9a3
mertmutludevelopment <155536158+mertmutludevelopment@users.noreply.github.com>
2024-07-17 20:36:00 +0300

    feat: update files

---
+++ Para.Api/Startup.cs
@@ -0,0 +1,44 @@
+using FluentValidation.AspNetCore;
+using Microsoft.OpenApi.Models;
+
+namespace Para.Api;
+
+public class Startup
+{
+    public IConfiguration Configuration;
+    
+    public Startup(IConfiguration configuration)
+    {
+        this.Configuration = configuration;
+    }
+    
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews()
+            .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Startup>());
+        services.AddControllers();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Para.Api", Version = "v1" });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Para.Api v1"));
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
